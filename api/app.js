82449ffa0043fea45cecd5360306465deb19a058
Loic DIRINGER <loic.diringer@niji.fr>
2016-03-22 11:51:31 +0100

    Ajout du système d'environnement pour charger la conf + utilisation de la conf pour le port d'écoute du serveur express

--- api/app.js
+++ api/app.js
@@ -4,20 +4,26 @@
 let express = require('express');
 let app = express();
 
+// Configuration
+let env = require('./config/env.json');
+let config = require('./config/'+ env.name +'.json');
+
+// Server configuration
 app.use(express.json());
 app.use(express.urlencoded());
 let http = require('http').Server(app);
 
+// Logger
 let Logger = require('bunyan');
 let log = new Logger({
-  name: 'helloapi',
+  name: 'api-mockiji',
   streams: [
     {
       stream: process.stdout,
       level: 'debug'
     },
     {
-      path: 'logs/hello.log',
+      path: 'logs/api-mockiji.log',
       level: 'trace'
     }
   ]
@@ -28,7 +34,9 @@ let routes = require('./src/routes/index');
 routes(app);
 
 // Launch server
-http.listen('3030');
+http.listen(config.app_listening_port, function() {
+    log.info("Server listening on port: " + config.app_listening_port);
+});
 
 // Expose
 module.exports = app;
