81ffa18f00b87bf08b67e9d078a96c4da073949a
Loic DIRINGER <loic.diringer@niji.fr>
2016-03-23 15:37:25 +0100

    Ajout d'un test heartbeat sur /

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,12 @@ var MockCtrl = require('../controllers/MockCtrl');
 
 module.exports = function(pApp) {
 
+    // HeartBeat test
+    pApp.get('/', function(req, res) {
+        res.end('Mockiji server is alive!');
+	});
+
+    // Main route
     pApp.all('/api*', function(req, res) {
         new MockCtrl().buildResponse(req, res);
 	});
