06d150ff4ef5316cdac0215d757fbe467752b765
Lyrkan <twentyhundredounet@gmail.com>
2017-04-06 14:03:55 +0200

    Modify the start method so it returns a Promise object + add a stop method

--- src/index.js
+++ src/index.js
@@ -3,37 +3,83 @@
 const express = require('express');
 
 class Mockiji {
+  /**
+   * Constructor.
+   *
+   * @param {Object} options
+   * @param {Object} options.configuration - A configuration object
+   * @param {string} options.configFile - A path to a configuration file
+   */
   constructor(options = {}) {
     // Create Configuration and Logger
     this.Configuration = require('./utils/Configuration')(options);
     this.Logger = require('./utils/Logger')({
       Configuration: this.Configuration
     });
-  }
 
-  start() {
     // Server
-    const app = express();
+    this.app = express();
 
     // Server configuration
-    app.use(express.json());
-    app.use(express.urlencoded());
-    const http = require('http').Server(app);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded());
+    this.http = require('http').Server(this.app);
 
     // Routes
-    const routes = require('./routes/index');
-    routes({
+    this.routes = require('./routes/index');
+    this.routes({
       Configuration: this.Configuration,
       Logger: this.Logger,
-      app
+      app: this.app
     });
+  }
 
+  /**
+   * Start the server.
+   *
+   * @return {Promise}
+   */
+  start() {
     // Launch server
     const port = this.Configuration.get('port');
-    http.listen(
-      port,
-      () => { this.Logger.info("Server listening on port: " + port); }
+    return new Promise((resolve, reject) => {
+      if (this.http && this.http.listening) {
+        reject('Server is already listening');
+        return;
+      }
+
+      this.http.once('error', reject);
+      this.http.listen(port, resolve);
+    }).then(
+      () => {this.Logger.info(`Server listening on port ${port}`); },
+      (error) => {
+        this.Logger.error(`Could not start server on port ${port}`);
+        throw error;
+      }
     );
+  }
+
+  /**
+   * Stop the server.
+   *
+   * @return {Promise}
+   */
+  stop() {
+    return new Promise((resolve, reject) => {
+      if (!this.http || !this.http.listening) {
+        reject('Server is not currently listening');
+        return;
+      }
+
+      this.http.once('error', reject);
+      this.http.close(port, resolve);
+    }).then(
+      () => {this.Logger.info(`Server stopped listening`); },
+      (error) => {
+        this.Logger.error(`Could not stop server`);
+        throw error;
+      }
+    )
   }
 }
 
