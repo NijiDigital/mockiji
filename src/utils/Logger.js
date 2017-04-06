2d68972b80a65b5b926256c4bd6d98700ae294c2
Lyrkan <twentyhundredounet@gmail.com>
2017-04-06 20:54:51 +0200

    Logs: Check for each stream if the filepath exists, create it otherwise

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const bunyan = require('bunyan');
+const path = require('path');
+const fs = require('fs');
 const argv = require('yargs').argv;
 
 function initLogger({Configuration}) {
@@ -25,6 +27,16 @@ function initLogger({Configuration}) {
       }
     } else {
       streams.push(loggerConfig);
+    }
+  }
+
+  // Create logs directory for each stream if needed
+  for (let stream of streams) {
+    if (stream.path) {
+      const logsDirectory = path.dirname(stream.path);
+      if (!fs.existsSync(logsDirectory)) {
+        fs.mkdirSync(logsDirectory);
+      }
     }
   }
 
